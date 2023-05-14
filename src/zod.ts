4b949c4feeef55e9b78b8d2e497cbe3b5728d5c7
lorefnon <lorefnon@gmail.com>
2023-05-14 18:12:45 +0530

    Add preliminary zod integration for runtime validation

---
+++ src/zod.ts
@@ -0,0 +1,32 @@
+import * as z from "zod";
+
+interface ZServiceSpec {
+  [key: string]: z.ZodFunction<z.AnyZodTuple, z.ZodTypeAny>
+}
+
+type ZServiceImpl<TSpec extends ZServiceSpec> = {
+  [key in keyof TSpec]: Parameters<TSpec[key]["implement"]>[0]
+}
+
+
+export class ZService<TSpec extends ZServiceSpec> {
+  static define<TSpec extends ZServiceSpec>(spec: TSpec) {
+    return new ZService(spec)
+  }
+
+  private constructor(public spec: TSpec) {}
+
+  implement<TCtx extends {}>(factory: (ctx: TCtx) => ZServiceImpl<TSpec>) {
+    return this.partiallyImplement<TCtx, ZServiceImpl<TSpec>>(factory)
+  }
+
+  partiallyImplement<TCtx extends {}, TImpl extends Partial<ZServiceImpl<TSpec>>>(factory: (ctx: TCtx) => TImpl) {
+    return (ctx: TCtx) => {
+      return Object.fromEntries(Object
+        .entries(factory(ctx))
+        .map(([key, fn]) => {
+          return [key, fn ? this.spec[key]?.implement(fn) : undefined]
+        })) as TImpl 
+    }
+  }
+}
